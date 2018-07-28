2ac0492bddf8dceab02b4f6c09a7ecb780f3ac6a
JoniAguero <aguerojonathanivan@gmail.com>
2018-07-28 17:19:36 -0300

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './todo-list/main/main.component';
+import { ItemComponent } from './todo-list/item/item.component';
+import { HeaderComponent } from './todo-list/header/header.component';
+import { FooterTodoComponent } from './todo-list/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainComponent,
+    ItemComponent,
+    HeaderComponent,
+    FooterTodoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-list/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './footer.component.html'
+})
+export class FooterTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
